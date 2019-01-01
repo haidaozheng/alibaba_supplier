94ec824f83fd1b918ab222210757dace5ecb8b46
haidaozheng <40845425+haidaozheng@users.noreply.github.com>
2019-01-01 10:39:34 +0800

    Update settings.py

--- alibaba_test/alibaba_test/settings.py
+++ alibaba_test/alibaba_test/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'alibaba_test.spiders'
 #USER_AGENT = 'alibaba_test (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = False    #关闭检查rebots
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.5
+DOWNLOAD_DELAY = 0.5    #延迟0.5秒
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
